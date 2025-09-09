8a7909c9f20c48a6315c1ea4dd2ce6781ad5c29d
Ajay Devarakonda <ajaydevarakonda150@gmail.com>
2025-09-09 12:34:49 +0530

    science

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -29,7 +29,6 @@ exports.sendNotificationToAll = async (req, res) => {
     res.status(200).json({
       success: response.successCount,
       failed: response.failureCount,
-      responses : response.responses
     });
   } catch (err) {
     res.status(500).json({ error: err.message });
